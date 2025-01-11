d708def40c08421312509a55d9d1cb02bf1bb33e
Abdul Ohab <ohabchoton@gmail.com>
2025-01-12 00:37:00 +0600

    after clickable category items

--- category/views.py
+++ category/views.py
@@ -1,27 +1,35 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,get_object_or_404
 from django.http import HttpResponse
-from home.models import *
+from home.models import Blogs ,Category
 
 # Create your views here.
 def categoryPage(request,category_id):
     #fetch the posts that belogns to the category with id category_id
     posts=Blogs.objects.filter(status=1,category=category_id)
+    categories = Category.objects.all()
     
     # when don't find the categoryid jus go back homepage
     try:
         category=Category.objects.get(pk=category_id)
-    except:
+    except Category.DoesNotExist:
         return redirect('homePage')
     context={
         'posts':posts,
-        'category':category
-
+        'category':category,
+        'categories':categories,
     }
     return render(request, 'category.html',context)
 
 
-def blogs(request,slug):
-    return render(request,'blogs.html')
+
+#this is for slug when you visit with slug , then show with categories
+def blogs(request, slug):
+    categories = Category.objects.all()
+    context = {
+        'categories': categories,
+    }
+    return render(request, 'blogs.html', context)
+
 
    
    
